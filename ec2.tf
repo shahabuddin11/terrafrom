64a30410ceb85513c98554d71bcc320acfc7c10a
shahabuddin11 <36933481+shahabuddin11@users.noreply.github.com>
2020-12-08 13:48:28 +0530

    Update ec2.tf
    comment key

--- ec2.tf
+++ ec2.tf
@@ -1,20 +1,20 @@
 
 #Create a ssh through the terraform
-resource "tls_private_key" "dev" {
-  algorithm = "RSA"
- rsa_bits  = 4096
-}
-resource "aws_key_pair" "generated_key" {
- key_name   = var.key_name
- public_key = tls_private_key.dev.public_key_openssh
-}
-# If you want to used existing key associated ith the instance
+# resource "tls_private_key" "dev" {
+#   algorithm = "RSA"
+#  rsa_bits  = 4096
+# }
+# resource "aws_key_pair" "generated_key" {
+#  key_name   = var.key_name
+#  public_key = tls_private_key.dev.public_key_openssh
+# }
+# If you want to used existing key associated with  the instance
 #please comment the above code and pass key name into the
 #key name
 resource "aws_instance" "test" {
   ami = lookup(var.AMI, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name = aws_key_pair.generated_key.key_name #keyname 
+  key_name = "test"#aws_key_pair.generated_key.key_name #keyname 
   # VPC
   subnet_id = aws_subnet.dev-subnet-public-1.id
   # Security Group
